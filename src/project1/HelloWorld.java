46268764521cfa0ff9f4df844ee75c6244881b91
22jy0123 <22jy0123@PC29.jynet2.jec.ac.jp>
2023-10-04 11:51:30 +0900

    マージした

--- src/project1/HelloWorld.java
+++ src/project1/HelloWorld.java
@@ -5,15 +5,15 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello!!!!");
 		System.out.println("aaaaa");
-<<<<<<< HEAD
+
 		System.out.println("bbbbb");
 		System.out.println("cccc");
 		System.out.println("NNN");
-=======
+
 		System.out.println("zzzzz");
 		System.out.println("fffff");
 		System.out.println("ababa");
->>>>>>> refs/heads/master
+
 
 	}
 
